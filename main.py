d1199cbc914f00f93b804181a5b23f8a5412b214
Deeper22 <s1103922@studenti.univpm.it>
2024-05-14 13:15:44 +0200

    Completata scelta primo piatto

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import sys
-
+import time
 
 class Utente():
     def __init__(self):
@@ -17,7 +17,6 @@ class Utente():
                 print('Errore! Inserisci numero di 7 cifre')
                 matr = input('Inserisci matricola: ')
 
-
         #INSERIMENTO E CONTROLLO NOME
         nome = input('Inserisci nome: ')
         ControlloSTR(nome)
@@ -33,36 +32,40 @@ class Utente():
         ControlloMTR(matricola)
         self.matricola = matricola
 
+
+    #CONTROLLO DISPONIBILITA POSTI IN MENSA
     def controlla_posto(self):
+
+        time.sleep(1)
+
         if mensa.posti_disponibili == 0:
-            print('Nessun posto disponibile')
+            print('\nNessun posto disponibile')
             sys.exit()
         else:
             mensa.posti_disponibili -= 1
-            print('Posto disponibile!\nPosti ancora disponibili: ' + str(mensa.posti_disponibili))
+            print('\nPosto disponibile!\nPosti ancora disponibili: ' + str(mensa.posti_disponibili))
 
-    #def scelta(self):
+
+    #SCELTA DEI PIATTI DA MANGIARE
+    def scelta(self):
+
+        time.sleep(2)
+        print('\nScegli primo piatto: ')
+
+        for i in range(len(menu.primo)):
+            print(str(i+1) + ') ' + menu.primo[i])
+
+        time.sleep(1)
+        primo_scelto = input('Digita il numero del piatto desiderato:')
+        while not (primo_scelto.isdigit() and 1 <= int(primo_scelto) <= len(menu.primo)):
+            primo_scelto = input('Piatto non accettato. Riprova: ')
+        print('Hai scelto ' + (menu.primo[int(primo_scelto)-1]))
 
 
 
 class Mensa():
     def __init__(self):
         self.posti_disponibili = 5
-
-
-
-
-
-utente1 = Utente()
-
-mensa = Mensa()
-utente1.controlla_posto()
-
-
-
-
-
-
 
 
 class Menu():
@@ -72,9 +75,16 @@ class Menu():
         self.contorno = ['Carote', 'Insalata', 'Finocchi']
 
 
+
+utente1 = Utente()
+
+mensa = Mensa()
+
+utente1.controlla_posto()
+
 menu = Menu()
 
-print(menu.primo[1])
+utente1.scelta()
 
 
 
