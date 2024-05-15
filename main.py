630625bdda16a02963f0ae98f917c06aa6d9444c
Deeper22 <s1103922@studenti.univpm.it>
2024-05-15 13:20:24 +0200

    Problema pickle

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import sys
 import time
+import pickle
 
 class Utente():
     def __init__(self):
@@ -43,10 +44,12 @@ class Utente():
             sys.exit()
         else:
             mensa.posti_disponibili -= 1
+            #mensa.salva_stato()
             print('\nPosto disponibile!\nPosti ancora disponibili: ' + str(mensa.posti_disponibili))
 
 
     #SCELTA DEI PIATTI DA MANGIARE
+
     def scelta(self):
 
         time.sleep(2)
@@ -59,20 +62,42 @@ class Utente():
         primo_scelto = input('Digita il numero del piatto desiderato: ')
         while not (primo_scelto.isdigit() and 1 <= int(primo_scelto) <= len(menu.primo)):
             primo_scelto = input('Piatto non accettato. Riprova: ')
+
         print('Hai scelto ' + (menu.primo[int(primo_scelto)-1]))
 
 
 
 class Mensa():
+
     def __init__(self):
         self.posti_disponibili = 5
+    # def __init__(self):
+    #     self.file_path = 'mensa.pkl'
+    #     self.posti_disponibili = self.carica_stato()
+    #
+    # def carica_stato(self):
+    #     try:
+    #         with open(self.file_path, 'r+') as file:
+    #             return pickle.load(file)
+    #     except FileNotFoundError:
+    #         return 5  # Valore di default se il file non esiste
+    #
+    # def salva_stato(self):
+    #     with open(self.file_path, 'r+') as file:
+    #         pickle.dump(self.posti_disponibili, file)
+
 
 
 class Menu():
     def __init__(self):
-        self.primo = ['Pasta', 'Zuppa', 'Riso']
+        self.primo = ['Pasta', 'Zuppa', 'Riso','Pomodoro']
         self.secondo = ['Maiale', 'Manzo', 'Mozzarella']
         self.contorno = ['Carote', 'Insalata', 'Finocchi']
+
+        self.contaprimo = [0 for x in self.primo]
+        self.contasecondo = [0 for x in self.secondo]
+        self.contacontorno = [0 for x in self.contorno]
+
 
 
 
