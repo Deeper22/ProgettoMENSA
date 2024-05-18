cb0791e61f0875180907036158afba52b5947bce
Deeper22 <s1103922@studenti.univpm.it>
2024-05-18 12:41:28 +0200

    Completato (senza admin)

--- main.py
+++ main.py
@@ -58,8 +58,14 @@ class Utente():
             print('\nPosto disponibile!\nPosti ancora disponibili: ' + str(mensa.posti_disponibili))
 
     def aggiornaPickle(self, nome_file, vett_pickle):
-        with open(nome_file+'.pkl', 'r+b') as nome_file:
+        with open(nome_file+'.pkl', 'rb+') as nome_file:
+
             vett_temp = pickle.load(nome_file)
+            if len(vett_temp) != len(vett_pickle):
+                nome_file.seek(0)   #Si porta il puntatore del file all'inizio per sovrascrivere
+                nome_file.truncate()   #Svuota il file
+                vett_temp = [0] * len(vett_pickle)
+                pickle.dump(vett_temp, nome_file)
             nome_file.seek(0)
             vett_pickle = [x + y for x, y in zip(vett_temp, vett_pickle)]
             pickle.dump(vett_pickle, nome_file)
@@ -70,18 +76,18 @@ class Utente():
 
         time.sleep(1)
 
-        vettoreprimi, primo_scelto = self.scelta(menu.primo, menu.contaprimo, 'primo')
+        vettoreprimi, primo_scelto = self.scelta(menu.primi, menu.contaprimo, 'primo')
 
-        vettoresecondi, secondo_scelto = self.scelta(menu.secondo, menu.contasecondo, 'secondo')
+        vettoresecondi, secondo_scelto = self.scelta(menu.secondi, menu.contasecondo, 'secondo')
 
-        vettorecontorni, contorno_scelto = self.scelta(menu.contorno, menu.contacontorno, 'contorno')
+        vettorecontorni, contorno_scelto = self.scelta(menu.contorni, menu.contacontorno, 'contorno')
 
-        print('\nHai scelto: {}, {}, {}.'.format(menu.primo[int(primo_scelto)-1],menu.secondo[int(secondo_scelto)-1],menu.contorno[int(contorno_scelto)-1]))
+        print('\nHai scelto: {}, {}, {}.'.format(menu.primi[int(primo_scelto) - 1], menu.secondi[int(secondo_scelto) - 1], menu.contorni[int(contorno_scelto) - 1]))
         conferma = input('\nInviare ordine? (si/no): ')
         while conferma != 'si' and conferma != 'no':
             conferma = input('Risposta non valida. Riprova: ')
         if conferma == 'si':
-            print('Ordine invitato!')
+            print('Ordine inviato!')
             self.aggiornaPickle('primi', vettoreprimi)
             self.aggiornaPickle('secondi', vettoresecondi)
             self.aggiornaPickle('contorni', vettorecontorni)
@@ -113,16 +119,26 @@ class Mensa():
         with open('posti.pkl', 'rb') as posti:
             self.posti_disponibili = pickle.load(posti)
 
+    def stampa_ordini(self):
+        pasti = ['primi', 'secondi', 'contorni']
+        for pasto in pasti:  # Iteriamo sui nomi dei pasti
+            file_path = pasto + '.pkl'
+            print(f"\nOrdini per i {pasto}:")
+            with open(file_path, 'rb') as file_ordine:
+                vett_ordini = pickle.load(file_ordine)
+                for idx, ordine in enumerate(vett_ordini):
+                    print(f"{ordine}x {menu.__dict__[pasto][idx]}")
+
 
 class Menu():
     def __init__(self):
-        self.primo = ['Pasta', 'Zuppa', 'Riso','Pomodoro', 'Risotto']
-        self.secondo = ['Maiale', 'Manzo', 'Mozzarella', 'Funghi']
-        self.contorno = ['Carote', 'Insalata', 'Finocchi', 'Patate']
+        self.primi = ['Pasta', 'Zuppa', 'Riso']
+        self.secondi = ['Maiale', 'Manzo', 'Mozzarella', 'Funghi', 'Carciofi']
+        self.contorni = ['Carote', 'Insalata', 'Finocchi', 'Patate']
 
-        self.contaprimo = [0 for x in self.primo]
-        self.contasecondo = [0 for x in self.secondo]
-        self.contacontorno = [0 for x in self.contorno]
+        self.contaprimo = [0 for x in self.primi]
+        self.contasecondo = [0 for x in self.secondi]
+        self.contacontorno = [0 for x in self.contorni]
 
 
 utente1 = Utente()
@@ -139,15 +155,17 @@ utente1.riepilogo_salvataggio()
 # print(menu.contasecondo)
 # print(menu.contacontorno)
 
+#
+# with open('primi.pkl', 'rb') as file_primi:
+#     print(pickle.load(file_primi))
+#
+# with open('secondi.pkl', 'rb') as file_secondi:
+#     print(pickle.load(file_secondi))
+#
+# with open('contorni.pkl', 'rb') as file_contorni:
+#     print(pickle.load(file_contorni))
 
-with open('primi.pkl', 'rb') as file_primi:
-    print(pickle.load(file_primi))
-
-with open('secondi.pkl', 'rb') as file_secondi:
-    print(pickle.load(file_secondi))
-
-with open('contorni.pkl', 'rb') as file_contorni:
-    print(pickle.load(file_contorni))
+mensa.stampa_ordini()
 
 
 
