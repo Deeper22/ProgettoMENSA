6a5622976b77827e89e964c381e138dc9c2a5ea4
Deeper22 <s1103922@studenti.univpm.it>
2024-05-28 13:01:27 +0200

    Menù utente, reset utenti

--- Controllo numeri in stringa.py
+++
@@ -1,5 +0,0 @@
-self.Nome = Nome
-        Nome = input()
-        while any(char.isdigit() for char in Nome):
-            print('Errore! Inserire stringa')
-            Nome = input()
--- SimpleGUI.py
+++
@@ -1,7 +0,0 @@
-# hello_world.py
-
-import PySimpleGUI as sg
-
-sg.Window(title="Hello World", layout=[[]], margins=(100, 50)).read()
-
-sg.Window(title="Mensa UNIVPM", layout=[[]], margins=(200, 150)).read()
